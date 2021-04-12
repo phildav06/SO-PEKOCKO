aef6c965d70aeba9e81c40730448d545f494784b
Philippe DAVID <philippedavid.phd@gmail.com>
2021-04-12 18:45:07 +0200

    Créa app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
 const express = require('express');
+require('dotenv').config();
+const helmet = require('helmet');
+// const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 const app = express();
+app.use(helmet());
 
-app.use((req, res) => {
-    res.json({message: 'Votre requête a bien été reçue !'});
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytpd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
+
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
