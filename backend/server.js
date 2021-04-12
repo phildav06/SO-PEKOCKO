ee0c04e27c41df8567f8844f25954f43b479f825
Philippe DAVID <philippedavid.phd@gmail.com>
2021-04-12 18:46:40 +0200

    Créa serveur.js

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,11 @@ const normalizePort = val => {
   }
   return false;
 };
+// la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+// la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
